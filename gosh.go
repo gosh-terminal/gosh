151602ff479aadd9e7e472eadaabf3c093362584
Sean Hellum <seanhellum45@gmail.com>
2019-10-15 02:37:01 +0000

    init

---
+++ gosh.go
@@ -0,0 +1,20 @@
+package main
+import (
+    "fmt"
+    "os"
+    "strings"
+    "bufio"
+)
+func main() {
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Println("Welcome to the Sean Shell!")
+    fmt.Println("--------------------------")
+    for {
+        fmt.Print(">>> ")
+        text,_ := reader.ReadString('\n')
+        text = strings.Replace(text, "\n", "", -1)
+        if strings.Compare("help",text) == 0 {
+            fmt.Println("Commands:\nhelp: displays this help screen")
+        }
+    }
+}
