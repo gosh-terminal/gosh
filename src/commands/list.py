353df34b44510a2791246ac1abac95e5adaf82c5
Sean Hellum <seanhellum45@gmail.com>
2019-10-22 21:33:09 +0000

    edit listall

--- src/commands/list.py
+++ src/commands/list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 import os
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
+print("----------------------------------")
 for i in files:
     char = ' '
-    if len(i) < 9:
-        z = 9 - len(i) - 2
-    else:
-        z = 1
+    z = 34 - len(i) - 3
     spaces = z*char
     item = "| " + i + spaces + "|"
-    print(" --------")
     print(item)
-print(" --------")
+print("----------------------------------")
