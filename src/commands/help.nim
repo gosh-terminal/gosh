4001693222981de9b51fc681cd3ee049aa1fea28
Sean Hellum <seanhellum45@gmail.com>
2019-10-22 22:19:20 +0000

    edits

--- src/commands/help.nim
+++ src/commands/help.nim
@@ -1,4 +1,4 @@
 import terminal
 setForeGroundColor(fgGreen)
-echo "Commands:\nhelp: displays this help screen\nexit: exits the terminal\nlistall: shows files in current directory"
+echo "Commands:\nhelp: displays this help screen\nexit: exits the terminal"
 setForeGroundColor(fgDefault)
--- src/gosh.go
+++ src/gosh.go
@@ -7,38 +7,31 @@ import (
     "os/exec"
     "log"
 )
+func executeCommand(theCommand string) {
+	cmd := exec.Command(theCommand)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Print(err)
+	}
+}
 func main() {
     reader := bufio.NewReader(os.Stdin)
     fmt.Println("Welcome to gosh the Go Shell!")
     fmt.Println("-----------------------------")
     for {
-        cmd := exec.Command("./src/commands/bin/prompt")
-            cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-            if err := cmd.Run(); err != nil {
-				log.Print(err)
-            }
+        executeCommand("./src/commands/bin/prompt")
         command,_ := reader.ReadString('\n')
         command = strings.Replace(command, "\n", "", -1)
         if strings.Compare("help",command) == 0 {
-			cmd := exec.Command("./src/commands/bin/help")
-            cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-            if err := cmd.Run(); err != nil {
-				log.Print(err)
-            }
+			executeCommand("./src/commands/bin/help")
         } else if strings.Compare("exit",command) == 0 {
             break;
-		} else if(strings.Compare("listall", command)==0) {
-			cmd := exec.Command("./src/commands/list.py")
-            cmd.Stdout = os.Stdout
-            cmd.Stderr = os.Stderr
-            if err := cmd.Run(); err != nil {
-                log.Print(err)
-            }
+		} else if(strings.Compare("ls", command)==0) {
+			executeCommand("./src/commands/list.py")
 		} else if(strings.Compare("",command) == 0) {
 			continue;
-		}else {
+		} else {
 			cmd := exec.Command(command)
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
