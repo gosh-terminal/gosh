bf0ca6323c816fcfcaf2d92df8c39dd8181d6577
Sean Hellum <seanhellum45@gmail.com>
2019-10-22 02:03:15 +0000

    change color of help

--- src/commands/help.nim
+++ src/commands/help.nim
@@ -1,4 +1,4 @@
 import terminal
-setForeGroundColor(fgCyan)
+setForeGroundColor(fgGreen)
 echo "Commands:\nhelp: displays this help screen\nexit: exits the terminal\nlistall: shows files in current directory"
 setForeGroundColor(fgDefault)
