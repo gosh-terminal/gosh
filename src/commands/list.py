7a8a93b19ab1bc38e62f274c10b90a63e74214e4
Sean Hellum <seanhellum45@gmail.com>
2019-11-14 18:19:23 +0000

    format list.py

--- src/commands/list.py
+++ src/commands/list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import os
-files1 = [(f,True) for f in os.listdir('.') if os.path.isfile(f)]
-files2 = [(f,False) for f in os.listdir('.') if os.path.isdir(f)]
+
+files1 = [(f, True) for f in os.listdir('.') if os.path.isfile(f)]
+files2 = [(f, False) for f in os.listdir('.') if os.path.isdir(f)]
 files = files1 + files2
 print("----------------------------------")
 for i in files:
@@ -11,12 +12,12 @@ for i in files:
     spaces = z*char
     item = "| " + i[0] + spaces + "|"
     if i[1] == True:
-        print(item[:1],end="")
-        print(item[1:-2],end="")
+        print(item[:1], end="")
+        print(item[1:-2], end="")
     else:
-        print(item[:1],end="")
+        print(item[:1], end="")
         os.system("/workspace/gosh/src/commands/bin/makeCyan")
-        print(item[1:-2],end="")
+        print(item[1:-2], end="")
     print(item[-2:])
     os.system("/workspace/gosh/src/commands/bin/resetColor")
 print("----------------------------------")
