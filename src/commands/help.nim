c775e7ccc48f0805c0e2727843a29f7b89d67e19
Sean Hellum <seanhellum45@gmail.com>
2019-11-14 22:00:38 +0000

    update help command colors

--- src/commands/help.nim
+++ src/commands/help.nim
@@ -1,4 +1,6 @@
 import terminal
-setForeGroundColor(fgGreen)
-echo "Commands:\nhelp: displays this help screen\nexit: exits the terminal"
+setForeGroundColor(fgMagenta)
+echo "Commands:"
+setForegroundColor(fgYellow)
+echo "help: displays this help screen\nexit: exits the terminal\nhistory: displays commands you have previously run"
 setForeGroundColor(fgDefault)
