47a36e86618a83b3211758f32b6482101c58b0c8
Sean Hellum <seanhellum45@gmail.com>
2019-11-15 20:28:02 +0000

    edot

--- gosh.go
+++ gosh.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/gookit/color"
-	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
@@ -54,27 +53,6 @@ func main() {
 			updateHistory(command)
 		}
 	}
-}
-func ls() {
-	files, err := ioutil.ReadDir(".")
-	if err != nil {
-		log.Fatal(err)
-	}
-	blue := color.FgCyan.Render
-	yellow := color.FgYellow.Render
-	fmt.Println("----------------------------------")
-	for _, file := range files {
-		z := 34 - len(file.Name()) - 3
-		spaces := strings.Repeat(" ", z)
-		if file.IsDir() {
-			fmt.Printf("| %s%s|\n", blue(file.Name()), spaces)
-		} else if file.Mode().String() == "-rwxr-xr-x" {
-			fmt.Printf("| %s%s|\n", yellow(file.Name()), spaces)
-		} else {
-			fmt.Printf("| %s%s|\n", file.Name(), spaces)
-		}
-	}
-	fmt.Println("----------------------------------")
 }
 func history() {
 	file, _ := os.Open("/workspace/gosh/data/history.txt")
