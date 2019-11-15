31ea65119e85fbe4e1ce557de941fe6b24be840f
Sean Hellum <seanhellum45@gmail.com>
2019-11-15 20:22:43 +0000

    refactor clearHist

--- gosh.go
+++ gosh.go
@@ -104,11 +104,6 @@ func printError(err error) {
 		os.Stderr.WriteString(fmt.Sprintf("%s\n", err.Error()))
 	}
 }
-func clearHistory() {
-	f, _ := os.OpenFile("/workspace/gosh/data/history.txt",
-		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	f.Truncate(0)
-}
 func updateHistory(command string) {
 	f, err := os.OpenFile("/workspace/gosh/data/history.txt",
 		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
