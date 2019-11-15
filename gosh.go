a2bd8b75bf00dcaaeb1688f209b83e74d855a006
Ted <thecla1@nilesk12.org>
2019-11-15 20:23:55 +0000

    changes

--- gosh.go
+++ gosh.go
@@ -55,10 +55,6 @@ func main() {
 		}
 	}
 }
-func help() {
-	color.FgMagenta.Println("Commands:")
-	color.FgYellow.Println("help: displays this help screen\nexit: exits the terminal\nhistory: displays commands you have previously run\nclearhist: clears your history")
-}
 func ls() {
 	files, err := ioutil.ReadDir(".")
 	if err != nil {
---
+++ help.go
@@ -0,0 +1,6 @@
+package main
+import "github.com/gookit/color"
+func help() {
+	color.FgMagenta.Println("Commands:")
+	color.FgYellow.Println("help: displays this help screen\nexit: exits the terminal\nhistory: displays commands you have previously run\nclearhist: clears your history")
+}
