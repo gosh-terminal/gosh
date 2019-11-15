93a888b6de76d3ec72feb6cd4e2b1f24a6d8b01b
Ted <thecla1@nilesk12.org>
2019-11-15 20:34:10 +0000

    update history changes

--- gosh.go
+++ gosh.go
@@ -66,17 +66,7 @@ func printError(err error) {
 		os.Stderr.WriteString(fmt.Sprintf("%s\n", err.Error()))
 	}
 }
-func updateHistory(command string) {
-	f, err := os.OpenFile("/workspace/gosh/data/history.txt",
-		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Println(err)
-	}
-	defer f.Close()
-	if _, err := f.WriteString("\n" + command); err != nil {
-		log.Println(err)
-	}
-}
+
 func getArg(commandString string) string {
 	var s []string = strings.Split(commandString, " ")
 	if len(s) > 1 {
