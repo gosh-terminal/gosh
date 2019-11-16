0c2f31f17d6e78686650beb3352f21553e5e49d1
Sean Hellum <seanhellum45@gmail.com>
2019-11-16 18:36:05 +0000

    add setup.sh

--- history.go
+++ history.go
@@ -7,12 +7,14 @@ import (
 	"log"
 )
 func clearHistory() {
-	f, _ := os.OpenFile("/workspace/gosh/data/history.txt",
+	var gopath string = os.Getenv("GOPATH")
+	f, _ := os.OpenFile(gopath + "/bin/history.txt",
 		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	f.Truncate(0)
 }
 func history() {
-	file, _ := os.Open("/workspace/gosh/data/history.txt")
+	var gopath string = os.Getenv("GOPATH")
+	file, _ := os.Open(gopath + "/bin/history.txt")
 	scanner := bufio.NewScanner(file)
 	var num int = 1
 	for scanner.Scan() {
@@ -25,7 +27,8 @@ func history() {
 }
 
 func updateHistory(command string) {
-	f, err := os.OpenFile("/workspace/gosh/data/history.txt",
+	var gopath string = os.Getenv("GOPATH")
+	f, err := os.OpenFile(gopath + "/bin/history.txt",
 		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Println(err)
---
+++ setup.sh
@@ -0,0 +1,4 @@
+go install
+touch history.txt
+mv history.txt $GOPATH/bin
+gosh
