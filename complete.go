3900d2e8f0a0237134e2ff0404718cfb71a7819c
Sean Hellum <seanhellum45@gmail.com>
2019-11-26 21:16:08 +0000

    add files and some hints

--- complete.go
+++ complete.go
@@ -20,7 +20,7 @@ func unique(intSlice []prompt.Suggest) []prompt.Suggest {
 	return list
 }
 func completer(d prompt.Document) []prompt.Suggest {
-	s := []prompt.Suggest{}
+	s := []prompt.Suggest{{Text: "help", Description: "Displays this help screen"}, {Text: "exit", Description: "Exit gosh"}, {Text: "history", Description: "displays commands you have previously run"}}
 	var gopath string = os.Getenv("GOPATH")
 	file, _ := os.Open(gopath + "/bin/history.txt")
 	scanner := bufio.NewScanner(file)
@@ -31,8 +31,12 @@ func completer(d prompt.Document) []prompt.Suggest {
 		s = append(s, prompt.Suggest{Text: scanner.Text()})
 	}
 	files, _ := ioutil.ReadDir("/usr/bin")
+	currentDir, _ := ioutil.ReadDir(".")
 	for _, file := range files {
-		s = append(s, prompt.Suggest{Text: file.Name()})
+		s = append(s, prompt.Suggest{Text: file.Name(), Description: "Command"})
+	}
+	for _, file := range currentDir {
+		s = append(s, prompt.Suggest{Text: file.Name(), Description: "File"})
 	}
 	return prompt.FilterHasPrefix(unique(s), d.GetWordBeforeCursor(), true)
 }
