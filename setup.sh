fae66ef93b534762c3b179879475a0f6edd52022
Sean Hellum <seanhellum45@gmail.com>
2019-11-26 21:09:56 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 git clone https://github.com/JesterOrNot/gosh.git
 cd gosh
+export GOPATH="$HOME"/go
+export PATH="$PATH:$GOROOT/bin:$GOPATH"/bin
 go get -v -t -d ./...
 go install
 touch history.txt
 mv history.txt "$GOPATH"/bin
-export GOPATH="$HOME"/go
-export PATH="$PATH:$GOROOT/bin:$GOPATH"/bin
 cd ..
 rm -rf gosh
 gosh
