8e775af41a8f760f1ed4c0192ac5a9e20d421c02
Sean Hellum <seanhellum45@gmail.com>
2019-11-27 17:42:52 +0000

    update

--- install.py
+++ install.py
@@ -6,12 +6,18 @@ def install():
     if "Linux" in platform.system():
         print("Installing for Linux")
         os.system("go install")
+        print("Test gosh")
+        os.system("$GOPATH/bin/gosh")
     elif "Darwin" in platform.system():
         print("Installing for OSX")
         os.system("go install")
+        print("Test gosh")
+        os.system("$GOPATH/bin/gosh")
     elif "Windows" in platform.system():
         print("Installing for Windows")
         os.system("go.exe install")
+        print("Test gosh")
+        os.system("%GOPATH/bin/gosh.exe")
     else:
         print("Unknown OS")
 
