10eeb2c1a478f4d43766b81c04049ff5f2c0ef8e
Restyled.io <commits@restyled.io>
2019-11-27 17:08:57 +0000

    Restyled by black

--- install.py
+++ install.py
@@ -6,11 +6,11 @@ def install():
     if "Linux" in platform.system():
         os.system("go build -o gosh *.go")
         os.system("go install")
-        os.system('gosh')
+        os.system("gosh")
     elif "Darwin" in platform.system():
         os.system("GOOS=darwin GOARCH=amd64 go build *.go")
         os.system("go install")
-        os.system('gosh')
+        os.system("gosh")
     elif "Windows" in platform.system():
         os.system("GOOS=windows GOARCH=amd64 go build -o gosh.exe *.go")
     else:
