2a265117f547fac26f9aabdc6932b64d5e9e48b1
Sean Hellum <seanhellum45@gmail.com>
2019-11-27 17:40:20 +0000

    logging

--- install.py
+++ install.py
@@ -4,12 +4,13 @@ import platform
 
 def install():
     if "Linux" in platform.system():
+        print("Installing for Linux")
         os.system("go install")
-        os.system("gosh")
     elif "Darwin" in platform.system():
+        print("Installing for OSX")
         os.system("go install")
-        os.system("gosh")
     elif "Windows" in platform.system():
+        print("Installing for Windows")
         os.system("go.exe install")
     else:
         print("Unknown OS")
