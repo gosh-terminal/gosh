2403fdf4057fa40012570422aa4929ad709856f9
Sean Hellum <seanhellum45@gmail.com>
2019-11-27 17:05:47 +0000

    update

--- install.py
+++ install.py
@@ -5,9 +5,9 @@ import platform
 def install():
     if "Linux" in platform.system():
         os.system("go build -o gosh *.go")
-    elif "macos" in platform.system():
+    elif "Darwin" in platform.system():
         os.system("GOOS=darwin GOARCH=amd64 go build *.go")
-    elif "windows" in platform.system():
+    elif "Windows" in platform.system():
         os.system("GOOS=windows GOARCH=amd64 go build -o gosh.exe *.go")
     else:
         print("Unknown OS")
