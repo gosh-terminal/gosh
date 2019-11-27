70ee3c98304dbbc9ee2204ba8fdd22624a5305d2
Restyled.io <commits@restyled.io>
2019-11-27 02:45:33 +0000

    Restyled by shellharden

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ go get -v -t -d ./...
 go install
 touch history.txt
 mv history.txt "$GOPATH"/bin
-export GOPATH=$HOME/go
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+export GOPATH="$HOME"/go
+export PATH="$PATH:$GOROOT/bin:$GOPATH"/bin
 cd ..
 rm -rf gosh
 gosh
