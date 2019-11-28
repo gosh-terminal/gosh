0948498a71e4e0bcff969c73e3fe542d23737aec
Sean Hellum <seanhellum45@gmail.com>
2019-11-28 19:39:11 +0000

    style history command

--- history.go
+++ history.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 	"strings"
+
+	"github.com/gookit/color"
 )
 
 func clearHistory() {
@@ -19,13 +21,22 @@ func history() {
 	file, _ := os.Open(gopath + "/bin/history.txt")
 	scanner := bufio.NewScanner(file)
 	var num = 1
+	fmt.Printf("   %s      %s\n", color.FgGreen.Render("#"), color.FgGreen.Render("command"))
+	fmt.Println(" ╭━━━━━━━━━━━━━━━━━━━╮")
 	for scanner.Scan() {
+		z := 13 - len(scanner.Text())
+		spaces := strings.Repeat(" ", z)
 		if strings.Compare(string(scanner.Text()), "") == 0 {
 			continue
 		}
-		fmt.Printf("%d %s\n", num, scanner.Text())
+		if num < 10 {
+			fmt.Printf(" │ %s  │ %s%s│\n", color.FgGreen.Render(num), scanner.Text(), spaces)
+		} else {
+			fmt.Printf(" │ %s │ %s%s│\n", color.FgGreen.Render(num), scanner.Text(), spaces)
+		}
 		num++
 	}
+	fmt.Println(" ╰━━━━━━━━━━━━━━━━━━━╯")
 }
 
 func updateHistory(command string) {
