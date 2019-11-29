546dcf2783c95d71d50c634ae4a10c9c10f21309
Restyled.io <commits@restyled.io>
2019-11-29 18:51:36 +0000

    Restyled by shellharden

--- build/setup.sh
+++ build/setup.sh
@@ -4,7 +4,7 @@ go get -v -t -d ./...
 go build -o gosh *.go
 export GOPATH="$HOME"/go
 export PATH="$PATH:$GOROOT/bin:$GOPATH"/bin
-mkdir $GOPATH/bin
+mkdir "$GOPATH"/bin
 touch history.txt
 mv history.txt "$GOPATH"/bin/
 mv gosh "$GOPATH"/bin/gosh
