2a05eacbbc0ec1a79c47ad4ecc2fef65ca5f7fc6
Sean Hellum <seanhellum45@gmail.com>
2019-11-29 18:51:13 +0000

    minor logic fix

--- build/setup.sh
+++ build/setup.sh
@@ -2,9 +2,9 @@
 cd gosh
 go get -v -t -d ./...
 go build -o gosh *.go
-mkdir $GOPATH/bin
 export GOPATH="$HOME"/go
 export PATH="$PATH:$GOROOT/bin:$GOPATH"/bin
+mkdir $GOPATH/bin
 touch history.txt
 mv history.txt "$GOPATH"/bin/
 mv gosh "$GOPATH"/bin/gosh
