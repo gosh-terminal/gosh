5db868f6a665ec5a218d9d3e986abd775c92cc08
Sean Hellum <seanhellum45@gmail.com>
2019-11-30 20:57:16 +0000

    add command line options

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"flags"
+	"fmt"
+	"os"
 )
 
 func main() {
-	version :=
+	if len(os.Args) == 1 {
+		shell()
+	}
+	if os.Args[1] == "-v" {
+        fmt.Println("v0.02-alpha")
+	}
 }
-// func printError(err error) {
-// 	if err != nil {
-// 		os.Stderr.WriteString(fmt.Sprintf("%s\n", err.Error()))
-// 	}
-// }
