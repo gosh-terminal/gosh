1d6ef2e14c17e4893df8ba1a52b1a6e5796403c3
Sean Hellum <seanhellum45@gmail.com>
2019-12-06 18:09:22 +0000

    fix bug

--- src/touch.go
+++ src/touch.go
@@ -16,7 +16,7 @@ func touch(filename string) {
 	for i := 1; i < len(filenames); i++ {
 		os.Create(filenames[i])
 	}
-	if len(dirname) >= 3 {
+	if len(filenames) >= 3 {
 		fmt.Printf("%s", color.FgYellow.Render("the files have been created! ✔\n"))
 	} else {
 		color.FgYellow.Println("file", filenames[1], "has been created! ✔")
