e2ea736b4bc55887588a8d4cd9d925a4b6058fed
Sean Hellum <seanhellum45@gmail.com>
2019-12-06 18:07:53 +0000

    add context related messages for `touch` command

--- src/touch.go
+++ src/touch.go
@@ -16,5 +16,9 @@ func touch(filename string) {
 	for i := 1; i < len(filenames); i++ {
 		os.Create(filenames[i])
 	}
-	fmt.Printf("%s", color.FgYellow.Render("the files have been created! ✔\n"))
+	if len(dirname) >= 3 {
+		fmt.Printf("%s", color.FgYellow.Render("the files have been created! ✔\n"))
+	} else {
+		color.FgYellow.Println("file", filenames[1], "has been created! ✔")
+	}
 }
