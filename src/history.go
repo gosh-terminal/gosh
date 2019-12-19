f16196a758e0e24650de098952a7be278033d605
Sean Hellum <seanhellum45@gmail.com>
2019-12-19 20:40:50 +0000

    refactor exit command to a function

--- src/history.go
+++ src/history.go
@@ -3,10 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/gookit/color"
 	"log"
 	"os"
 	"strings"
-	"github.com/gookit/color"
 )
 
 func clearHistory() string {
@@ -43,7 +43,7 @@ func history() {
 
 func updateHistory(command string) {
 	var gopath string = os.Getenv("GOPATH")
-	f, err := os.OpenFile(gopath + "/bin/history.txt",
+	f, err := os.OpenFile(gopath+"/bin/history.txt",
 		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Println(err)
